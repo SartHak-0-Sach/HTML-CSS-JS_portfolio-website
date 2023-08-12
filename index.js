f8515187c93f825eb737d0f560eabc4351d8cefa
Sarthak Sachdev <sarthaksachdev_me21b16_11@dtu.ac.in>
2023-08-13 01:27:39 +0530

    portfolio website updated

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
     $(window).scroll(function(){
-
+        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
         
+        // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
@@ -14,20 +15,25 @@ $(document).ready(function(){
         }
     });
 
+    // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
+    // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
 
+    // typing text animation script
     var typed = new Typed(".typing", {
         strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
         typeSpeed: 100,
@@ -42,6 +48,7 @@ $(document).ready(function(){
         loop: true
     });
 
+    // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
