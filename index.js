c1a61b45a52971abf3407fbfb956923a72abedca
Sarthak <saarthaksach@gmail.com>
2023-10-19 16:02:53 +0530

    Adding new components and changing name of files

--- index.js
+++
@@ -1,9 +0,0 @@
-// Convert square favicon to round using JavaScript
-const favicon = document.querySelector("link[rel='icon']");
-const roundedFavicon = document.createElement("link");
-roundedFavicon.rel = "icon";
-roundedFavicon.type = "image/x-icon";
-roundedFavicon.href = "favicon.jpg";
-
-favicon.parentNode.insertBefore(roundedFavicon, favicon);
-favicon.parentNode.removeChild(favicon);
