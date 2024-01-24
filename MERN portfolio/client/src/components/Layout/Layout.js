68c22ae1d8433035d21eaa3d02ffeb2bcc32dc85
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-24 10:30:54 +0530

    Add sidebar and profile pic

--- MERN portfolio/client/src/components/Layout/Layout.js
+++ MERN portfolio/client/src/components/Layout/Layout.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Home from '../../pages/Home/Home'
+import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
+import "./Layout.css";
+import Menus from '../Menus/Menus';
+
 const Layout = () => {
+    const [toggle, setToggle] = useState(true);
+
+    // change toggle
+    const handleToggle = () => {
+        setToggle(!toggle);
+    };
     return (
         <>
             <div className="sidebar-section">
-                <div className="sidebar">
+                <div className={toggle ? "sidebar-toggle sidebar" : "sidebar"}>
                     <div className="sidebar-toggle-icons">
-                        icon
+                        <Menus />
+                        <p onClick={handleToggle}>
+                            {
+                                toggle ? (<AiOutlineDoubleLeft size={30} />) : (<AiOutlineDoubleRight size={30} />)
+                            }
+                        </p>
                     </div>
                 </div>
                 <div className="container">
---
+++ MERN portfolio/client/src/components/Menus/Menus.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Menus.css";
+
+const Menus = () => {
+  return (
+    <>
+    <div className="navbar-profile-pic">
+        <img src="https://t4.ftcdn.net/jpg/03/64/21/11/360_F_364211147_1qgLVxv1Tcq0Ohz3FawUfrtONzz8nq3e.jpg" alt="profile pic" />
+    </div>
+    </>
+  )
+}
+
+export default Menus
